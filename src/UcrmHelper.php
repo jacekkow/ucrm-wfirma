14d3eeddc827c7d822ecbc7b371b13cb3e9ac108
Jacek Kowalski <Jacek@jacekk.info>
2023-06-14 12:34:14 +0200

    Fixes for UCRM 4.0

--- src/UcrmHelper.php
+++ src/UcrmHelper.php
@@ -79,7 +79,7 @@ class UcrmHelper {
 					throw new \RuntimeException('Failed to process event - event not found');
 				}
 
-				if (!is_int($event['entityId'])) {
+				if (!ctype_digit($event['entityId'])) {
 					throw new \RuntimeException('Failed to process event - invalid entity ID');
 				}
 
